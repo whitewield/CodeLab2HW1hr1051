a3e932bd5e251cdb21ed4ad8146dc5749694cb24
Hang <whitewield@126.com>
2017-09-13 11:20:35 -0400

    create pattern

---
+++ WarmUpHW/Assets/Scripts/CS_Global.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Global {
+
+
+	public enum Symbol {
+		Green,
+		Red,
+		Blue,
+		Yellow
+	}
+
+	public static class GlobalInfo {
+
+		public const int DISTANCE_SYMBOL = 1;
+	}
+}
+
+
